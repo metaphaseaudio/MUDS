bd8e26a8b63166d03108a7ee965b90f566736ff4
zappmatt <zappmatt@amazon.com>
2021-01-26 20:10:14 -0500

    adding install script for juce libs

---
+++ build_scripts/install_juce_libs_ubuntu.sh
@@ -0,0 +1,4 @@
+#sudo apt-get update
+sudo apt install -y libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev mesa-common-dev \
+  libasound2-dev freeglut3-dev libxcomposite-dev libxcb-util-dev libxcb-cursor-dev libxcb-keysyms1-dev libxcb-xkb-dev \
+  libxkbcommon-dev libxkbcommon-x11-dev pkg-config
