42341fc96843bb822ef41f81278ba65e8ee575cb
MrBZapp <info@metaphaseaudio.com>
2024-08-01 13:11:03 -0400

    adding new JUCE libs to the installation script

--- build_scripts/install_juce_libs_ubuntu.sh
+++ build_scripts/install_juce_libs_ubuntu.sh
@@ -1,4 +1,5 @@
 #sudo apt-get update
 sudo apt install -y libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev mesa-common-dev \
   libasound2-dev freeglut3-dev libxcomposite-dev libxcb-util-dev libxcb-cursor-dev libxcb-keysyms1-dev libxcb-xkb-dev \
-  libxkbcommon-dev libxkbcommon-x11-dev pkg-config
+  libxkbcommon-dev libxkbcommon-x11-dev pkg-config libglib2.0-dev libcairo-dev libsdl-pango-dev libgtkmm-3.0-dev \
+  libsqlite3-dev
